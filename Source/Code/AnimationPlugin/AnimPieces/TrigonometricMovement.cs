536ad694c06a014709e40f87205016d68b17da9d
mfep <serfozo.lorinc@gmail.com>
2016-09-26 20:59:22 +0200

    Add Trigonometric movement piece

---
+++ Source/Code/AnimationPlugin/AnimPieces/TrigonometricMovement.cs
@@ -0,0 +1,23 @@
+﻿using Duality;
+
+namespace MFEP.Duality.Plugins.Animation.AnimPieces
+{
+    public class TrigonometricMovement : IAnimPiece
+    {
+        public Vector2 Frequency { get; set; }
+        public Vector2 Amplitude { get; set; }
+        public Vector2 Phase { get; set; }
+
+        public void Initialize ()
+        {
+        }
+
+        public void Tick (float percent, GameObject gameObject)
+        {
+            gameObject.Transform?.MoveTo (new Vector2 (
+                Amplitude.X * MathF.Cos ( percent * MathF.TwoPi * Frequency.X + Phase.X ),
+                Amplitude.Y * MathF.Sin ( percent * MathF.TwoPi * Frequency.Y + Phase.Y )
+                ));
+        }
+    }
+}
---
+++ Source/Code/CorePlugin/TrigonometricMovementSample.cs
@@ -0,0 +1,18 @@
+﻿using Duality;
+using Duality.Editor;
+using MFEP.Duality.Plugins.Animation.AnimPieces;
+
+namespace MFEP.Duality.Plugins.Animation.Sample
+{
+    [EditorHintCategory (ResNames.EditorCategory)]
+    public class TrigonometricMovementSample : AnimResource
+    {
+        protected override AnimBuilder InitializePieces ()
+        {
+            return AnimBuilder.Start ()
+                .AddSingle (
+                    new TrigonometricMovement { Frequency = new Vector2 (1), Amplitude = new Vector2 (800) }, 4.0f
+                );
+        }
+    }
+}
