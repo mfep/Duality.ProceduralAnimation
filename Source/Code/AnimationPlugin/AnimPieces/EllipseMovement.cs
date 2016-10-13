aeb742c7bb703bf17e214240a83b5024072c36e4
mfep <serfozo.lorinc@gmail.com>
2016-10-13 21:17:42 +0200

    Documentation to the EllipseMovement and TrigonometricMovement2D classes

--- Source/Code/AnimationPlugin/AnimPieces/EllipseMovement.cs
+++ Source/Code/AnimationPlugin/AnimPieces/EllipseMovement.cs
@@ -1,22 +1,65 @@
 ﻿using Duality;
 using static Duality.MathF;
+using static MFEP.Duality.Plugins.Animation.Utils;
 
 namespace MFEP.Duality.Plugins.Animation.AnimPieces
 {
+	/// <summary>
+	///     Moves the gameObject on an elliptical arc. This path can be a circle/ellipse/circular arc depending on the
+	///     parameters.
+	/// </summary>
 	public class EllipseMovement : IAnimPiece
 	{
 		private Vector2 lastPos;
 
+		/// <summary>
+		///     The size of the path ellipse on the X axis.
+		///     Default is 200f
+		/// </summary>
 		public float RadiusX { get; set; } = 200f;
+
+		/// <summary>
+		///     RadiusX's multiplier function.
+		///     Default is Const1
+		/// </summary>
+		public SignalGen RadiusXGen { get; set; } = Const1;
+
+		/// <summary>
+		///     The size of the path ellipse on the Y axis.
+		///     Default is 200f
+		/// </summary>
 		public float RadiusY { get; set; } = 200f;
+
+		/// <summary>
+		///     RadiusY's multiplier function.
+		///     Default is Const1
+		/// </summary>
+		public SignalGen RadiusYGen { get; set; } = Const1;
+
+		/// <summary>
+		///     The starting phase angle on the path in radians. Determines the starting point on the arc. 0 means the most right
+		///     point, Pi/2 is the bottom one, and so along.
+		///     Default is 0f
+		/// </summary>
 		public float StartPhase { get; set; } = 0f;
+
+		/// <summary>
+		///     The ending phase angle on the path in radians.
+		///     Default is 0f
+		/// </summary>
 		public float EndPhase { get; set; } = TwoPi;
+
+		/// <summary>
+		///     Wheter the movement is combineable with other movement. Even if false, the movement takes place in local space.
+		///     Default is true
+		/// </summary>
 		public bool Relative { get; set; } = true;
 
 		public void Tick (float pc, GameObject gameObject)
 		{
-			var currentPhase = DegToRad (Lerp (StartPhase, EndPhase, pc));
-			var newPos = new Vector2 (RadiusX * Cos (currentPhase), RadiusY * Sin (currentPhase));
+			var currentPhase = Lerp (StartPhase, EndPhase, pc);
+			var newPos = new Vector2 (RadiusXGen (pc) * RadiusX * Cos (currentPhase),
+				RadiusYGen (pc) * RadiusY * Sin (currentPhase));
 			if (Relative) {
 				gameObject.Transform.MoveBy (newPos - lastPos);
 				lastPos = newPos;
--- Source/Code/AnimationPlugin/AnimPieces/TrigonometricMovement2D.cs
+++ Source/Code/AnimationPlugin/AnimPieces/TrigonometricMovement2D.cs
@@ -3,18 +3,72 @@ using static MFEP.Duality.Plugins.Animation.Utils;
 
 namespace MFEP.Duality.Plugins.Animation.AnimPieces
 {
+	/// <summary>
+	///     Provides more advanced controls over the trigonometric functions which move the gameObject. Simpler version is
+	///     EllipseMovement.
+	///     Consists of two trigonometric expression: one in the X, one in the Y direction.
+	/// </summary>
 	public class TrigonometricMovement2D : IAnimPiece
 	{
 		private Vector2 lastPos;
+
+		/// <summary>
+		///     The 'speed' of the trigonometric functions in the X and Y directions.
+		///     The frequency of 1 means one period over the animation.
+		///     Default is Vector2.One
+		/// </summary>
 		public Vector2 Frequency { get; set; } = Vector2.One;
+
+		/// <summary>
+		///     Frequency multiplier function in the X direction.
+		///     Default is Const1
+		/// </summary>
 		public SignalGen FreqGenX { get; set; } = Const1;
+
+		/// <summary>
+		///     Frequency multiplier function in the Y direction.
+		///     Default is Const1
+		/// </summary>
 		public SignalGen FreqGenY { get; set; } = Const1;
+
+		/// <summary>
+		///     The magnitude of the movement in X and Y direction.
+		///     Default is Vector2 (200f, 200f)
+		/// </summary>
 		public Vector2 Amplitude { get; set; } = Vector2.One * 200.0f;
+
+		/// <summary>
+		///     Amplitude multiplier function in the X direction.
+		///     Default is Const1
+		/// </summary>
 		public SignalGen AmpGenX { get; set; } = Const1;
+
+		/// <summary>
+		///     Amplitude multiplier function in the Y direction.
+		///     Default is Const1
+		/// </summary>
 		public SignalGen AmpGenY { get; set; } = Const1;
+
+		/// <summary>
+		///     The phase of the trigonometric expression in X and Y direction.
+		///     Default is Vector2.Zero
+		/// </summary>
 		public Vector2 Phase { get; set; }
+
+		/// <summary>
+		///     Phase multiplier function in the X direction.
+		/// </summary>
 		public SignalGen PhaseGenX { get; set; } = Const1;
+
+		/// <summary>
+		///     Phase multiplier function in the Y direction.
+		/// </summary>
 		public SignalGen PhaseGenY { get; set; } = Const1;
+
+		/// <summary>
+		///     Wheter the movement is combineable with other movement. Even if false, the movement takes place in local space.
+		///     Default is true
+		/// </summary>
 		public bool Relative { get; set; } = true;
 
 		public void Initialize ()
